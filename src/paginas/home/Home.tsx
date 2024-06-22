53190fa48e1a9841e137a966dbae668454bc2bdd
Eliana Morato <liamorato@hotmail.com>
2024-06-21 23:55:53 -0300

    Criando um projeto React pelo Vite e personalizando a estilização Criado um projeto React pelo vite usando yarn Configuração da App.tsx Criação da pagina index.html, Home.css e Home.tsx Personalização das paginas de estilo em css. Verificação da aplicação estar funcionando corretamente.

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+    return(
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://img.freepik.com/vetores-gratis/ilustracao-do-conceito-de-conta_114360-399.jpg?t=st=1718983327~exp=1718986927~hmac=f6fd9a3a54448f3581810b6641cf3493f325320ea6f076b35db284a63f845610&w=740" alt="Imagem Tela Inicial" className="img" />
+        </>
+    );
+}
+
+export default Home;
