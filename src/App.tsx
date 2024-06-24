267378783acdfecb6cd74a4ec39e5bdadd5cbd67
Eliana Morato <liamorato@hotmail.com>
2024-06-24 11:39:54 -0300

    React - Props e Hooks Construido props em React com TypeScript Construido hook useState em React com TypeScript Criada função somar para adicionar um número toda vez que clicar Construido hook useEffect em React com TypeScript Criado um alert que exibe na tela um aviso para parar de somar ao clicar em adicionar um número Criado um botão para concluir a tarefa com useEffect usando if dentro da função Construido renderização condicional em React com TypeScript Criado um botão para logar usando if ternário

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import './App.css'
 import Home from './paginas/home/Home'
 
-const App = () => {
+function App() {
   return (
-    <Home/>
+    <>
+      <Home titulo="home" texto="um texto aleatorio" cor="purple"/>
+    </>
+    
   )
 }
 
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,13 +1,81 @@
-import React from 'react';
-import './Home.css';
+import React, { useEffect, useState } from "react";
+import "./Home.css";
 
-const Home = () => {
-    return(
-        <>
-            <h1 className="titulo">Home</h1>
-            <img src="https://img.freepik.com/vetores-gratis/ilustracao-do-conceito-de-conta_114360-399.jpg?t=st=1718983327~exp=1718986927~hmac=f6fd9a3a54448f3581810b6641cf3493f325320ea6f076b35db284a63f845610&w=740" alt="Imagem Tela Inicial" className="img" />
-        </>
-    );
+interface myProps {
+  titulo: string;
+  texto: string;
+  cor: string;
+}
+
+function Home(props: myProps) {
+  //variavel que o react nao ta nem ai
+  //let contador = 1
+
+  {/*tarefa 2 - função para somar um numero ao clicar o botão*/}
+  //variavel que o react se importa
+  const [numero, setNumero] = useState(0);
+
+  function somar() {
+    setNumero(numero + 1);
+    console.log(numero);
+  }
+
+  // useEffect(função, dependencia)
+  // funcao: () => {}    -> arrow function (funcao anonima)
+  // dependencia: []
+
+  {/* alerta que aparece na tela para parar de somar */}
+  useEffect(() => {
+    alert("para de somar");
+  }, [numero]);
+
+  {/* tarefa 3 - clicar no botão para concluir a tarefa */}
+  const [completed, setCompleted] = useState(false);
+  const [tarefa, setTarefa] = useState("");
+
+  useEffect(() => {
+    if (completed) {
+      setTarefa("Parabéns! Você concluiu a tarefa!");
+    }
+  }, [completed]);
+
+  {/* tarefa 4 - clicar no botão para logar */}
+  const [logado, setLogado] = useState(false);
+
+  return (
+    <>
+      {/*tarefa 1*/}
+      <h1 style={{ fontSize: "64px", color: `${props.cor}` }}>
+        {props.titulo}
+      </h1>
+      <p>{props.texto}</p>
+      <hr/>
+
+      {/*tarefa 2*/}
+      <h1>{numero}</h1>
+      <button onClick={somar}>Adicionar + 1</button>
+      <hr/>
+
+      {/*tarefa 3*/}
+      <div>
+        <h1>Tarefa</h1>
+        <h3>{tarefa}</h3>
+        <p>Conclua a tarefa</p>
+        <button onClick={() => setCompleted(true)}>Concluir Tarefa</button>
+      </div>
+      <hr/>
+
+      {/*tarefa 4*/}
+      {/* if ternário */}
+      <div>
+        {logado == false ? (
+          <button onClick={() => {setLogado(true);}}>Clique aqui para logar</button>
+        ) : (
+          <h1> Parabéns, você está logado</h1>
+        )}
+      </div>
+    </>
+  );
 }
 
 export default Home;
