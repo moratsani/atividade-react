5fbbba604babc6422f4cd96acb8a5ae2fe5e240b
Eliana Morato <liamorato@hotmail.com>
2024-06-26 00:17:25 -0300

    Criando Rotas com o React Router Dom Instalada a biblioteca de roteamento para aplicações em React Feita a página teste de Login e sua respectiva pasta

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,42 +1,25 @@
-import React from "react";
-import "./Home.css";
+import { useNavigate, Link } from 'react-router-dom'
+import React from 'react'
 
-function Home() {
-  return (
-    <>
-      <div>
-        <div className="grid grid-cols-12 gap-2 m-4">
-          <div className="col-span-6 bg-green-300">container 1</div>
 
-          <div className="col-span-6 bg-yellow-200">Container 2</div>
+function Home(){
+    let navigate = useNavigate()
+    return (
+
+        <div>
+            <h2 className="text-slate-900 text-5xl  m-4">Home</h2>
+            <div>
+                <button type='submit'
+                    className='hover:underline mx-4'
+                    onClick={() => { navigate('/login') }}>
+                    Login useNavigate
+                </button>
+                <Link to='/login' className='hover:underline mx-4'>Login por Link</Link>
+            </div>
+
         </div>
 
-        <div className="grid grid-cols-12 gap-2 m-4">
-          <div className="col-span-8 bg-green-300">container 1</div>
-
-          <div className="col-span-4 bg-yellow-200">Container 2</div>
-        </div>
-
-        <div className="grid grid-cols-12 gap-2 m-4">
-          <div className="col-span-4 bg-green-300">container 1</div>
-
-          <div className="col-span-4 bg-yellow-200">Container 2</div>
-
-          <div className="col-span-4 bg-red-500">Container 3</div>
-        </div>
-
-        <div className="grid grid-cols-12 gap-2 m-4">
-          <div className="col-span-4 bg-green-300">container 1</div>
-
-          <div className="col-span-4 bg-yellow-200">Container 2</div>
-
-          <div className="col-span-2 bg-red-500">Container 3</div>
-
-          <div className="col-span-2 bg-blue-500">Container 4</div>
-        </div>
-      </div>
-    </>
-  );
+    )
 }
 
-export default Home;
+export default Home
