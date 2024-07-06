a6d09e2ab0252489977292aef1d6d716d8c165b8
Eliana Morato <liamorato@hotmail.com>
2024-07-06 14:01:54 -0300

    Construindo alerts personalizado com react-toastify
    Instalado toastAlerta com seguinte código: yarn add react-toastify@9.1.2
    Construída a pasta chamada util dentro da pasta src do projeto.
    Construído o componente: toastAlerta.ts
    Editado o arquivo: DeletarPostagem.tsx
    Editado o arquivo: FormularioPostagem.tsx
    Editado o arquivo: ListaPostagens.tsx
    Editado o arquivo: DeletarTema.tsx
    Editado o arquivo: FormularioTema.tsx
    Editado o arquivo: ListaTemas.tsx
    Editado o arquivo: AuthContext.tsx
    Editado o arquivo: Cadastro.tsx
    Editado o arquivo: NavBar.tsx
    Editado o arquivo: Home.tsx
    Editado o arquivo: Login.tsx
    Editado o arquivo: Perfil.tsx
    Editado o arquivo: App.tsx

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -20,7 +20,7 @@ function Navbar() {
       navbarComponent = (
         <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
           <div className="container flex justify-between text-lg">
-            <Link to='/home' className='text-2xl font-bold uppercase'>Blog Pessoal da Lia</Link>
+            <Link to='/home' className='text-2xl font-bold uppercase'>Blog Pessoal</Link>
 
             <div className='flex gap-4'>
               <Link to='/postagens' className='hover:underline'>Postagens</Link>
